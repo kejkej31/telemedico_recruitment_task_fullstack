74f6893cd01cbf18ed1f309c6984cbf6ce7d04bd
KejKej <me@kejkej.pl>
2024-07-21 03:06:24 +0200

    Add controller + route for getting exchange rates

--- src/App/Controller/ExchangeRatesController.php
+++ src/App/Controller/ExchangeRatesController.php
@@ -4,10 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Services\ExchangeRateService\BussinessExchangeRateServiceInterface;
 
 class ExchangeRatesController extends AbstractController
 {
+    protected $exchangeRatesService;
 
+    protected $logger = null;
 
+    public function __construct(
+        BussinessExchangeRateServiceInterface $exchangeRatesService,
+        LoggerInterface $logger
+    ) {
+        $this->exchangeRatesService = $exchangeRatesService;
+        $this->logger = $logger;
+    }
+
+    public function getExchangeRates(Request $request)
+    {
+        $date = new \DateTime($request->query->get('date', date('Y-m-d')));
+        try {
+            $exchangeRates = $this->exchangeRatesService->getExchangeRates($date);
+        } catch (\Exception $e) {
+            // KK: This should be handled better, we should diffenteriate between handled exceptions and unhandled
+            // Right now we don't display proper error on frontend anyway
+            return $this->json(['error' => $e->getMessage()], $e->getCode());
+        }
+        // KK: we intentionally omit "mid" value
+        // According to specification we don't need it on frontend, and it could be considered "sensitive"
+        return $this->json($exchangeRates);
+    }
 }
