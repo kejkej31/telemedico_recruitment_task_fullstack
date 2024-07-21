30ce877029c181dcc0f415b116a288299840bde0
KejKej <me@kejkej.pl>
2024-07-21 03:08:46 +0200

    Add loader component

---
+++ assets/js/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className={'text-center'}>
+            <span className="fa fa-spin fa-spinner fa-4x"></span>
+        </div>
+    )
+};
+
+export default Loader;
